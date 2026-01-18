7be3ae8e6e83a1c05d2ffbe4189326afc6cb6317
felichio <fsafaridis@hotmail.com>
2026-01-19 01:10:11 +0200

    add json dep

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
 #include <boost/beast/core.hpp>
+#include <nlohmann/json.hpp>
 
 int main(int argc, char *argv[])
 {
   std::cout << "bb" << std::endl;
   boost::asio::io_context io_context;
   boost::beast::flat_buffer buffer;
+
+  nlohmann::json t = {
+    {"kappa", "alg"}
+  };
+
+  std::cout << t.dump() << std::endl;
 }
