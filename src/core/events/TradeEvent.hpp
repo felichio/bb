7564ee7385a358ae88c0c0b00cb59eb205bf59a0
felichio <fsafaridis@hotmail.com>
2026-07-05 19:54:52 +0300

    add TradeEvent structure

---
+++ src/core/events/TradeEvent.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <string>
+#include <cstdint>
+#include <nlohmann/json.hpp>
+
+namespace bb
+{
+
+  struct TradeEvent
+  {
+    std::string type;
+    uint64_t eventTime;
+    std::string symbol;
+    uint64_t tradeId;
+    std::string price;
+    std::string quantity;
+    uint64_t tradeTime;
+    bool marketMaker;
+
+    explicit TradeEvent(const nlohmann::json& te)
+    {
+      type = te["e"].get<std::string>();
+      eventTime = te["E"].get<uint64_t>();
+      symbol = te["s"].get<std::string>();
+      tradeId = te["t"].get<uint64_t>();
+      price = te["p"].get<std::string>();
+      quantity = te["q"].get<std::string>();
+      tradeTime = te["T"].get<uint64_t>();
+      marketMaker = te["m"].get<bool>();
+    }
+
+    bool operator==(const TradeEvent &other) const
+    {
+      return type        == other.type      &&
+             eventTime   == other.eventTime &&
+             symbol      == other.symbol    &&
+             tradeId     == other.tradeId   &&
+             price       == other.price     &&
+             tradeTime   == other.tradeTime &&
+             marketMaker == other.marketMaker;
+    }
+
+    bool operator<(const TradeEvent &other) const
+    {
+      return eventTime > other.eventTime;
+    }
+
+  };
+
+} // bb
