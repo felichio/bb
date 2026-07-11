023c503371126892609cfd074b166dea79fd6a83
felichio <fsafaridis@hotmail.com>
2026-07-12 01:24:35 +0300

    intent as static member

--- src/core/producers/TradeEventProducer.cpp
+++ src/core/producers/TradeEventProducer.cpp
@@ -4,12 +4,31 @@
 
 namespace bb
 {
+
   template <typename T>
-  void bb::TradeEventProducer<T>::delegateEvent(const T& event)
+  TradeEventProducer<T>::TradeEventProducer(bb::EventQueue<T, 5> &eventQueue, bb::WebSocketClient<T> &webSocketClient)
+      : m_eq{eventQueue},
+        m_wsc{webSocketClient}
+  {
+    m_wsc.registerProducer(this);
+    m_wsc.connect();
+    m_wsc.subscribe(TradeEventProducer::intent);
+    m_wsc.run();
+  }
+
+  template <typename T>
+  void bb::TradeEventProducer<T>::delegateEvent(const T &event)
   {
     spdlog::info("[delegateEvent] {}", event);
     m_eq.push(event);
   }
+
+  template <typename T>
+  nlohmann::json TradeEventProducer<T>::intent = nlohmann::json::parse(R"({
+        "method" : "SUBSCRIBE",
+        "params" : ["btcusdt@trade"],
+        "id" : "1"
+      })");
 
   template class TradeEventProducer<TradeEvent>;
 } // bb
--- src/core/producers/TradeEventProducer.hpp
+++ src/core/producers/TradeEventProducer.hpp
@@ -9,25 +9,13 @@ namespace bb
   class TradeEventProducer : public IProducer<T>
   {
   public:
-    TradeEventProducer(bb::EventQueue<T, 5> &eventQueue, bb::WebSocketClient<T> &webSocketClient)
-        : m_eq{eventQueue},
-          m_wsc{webSocketClient}
-    {
-      m_wsc.registerProducer(this);
-
-      m_wsc.connect();
-      nlohmann::json intent = nlohmann::json::parse(R"({
-        "method" : "SUBSCRIBE",
-        "params" : ["btcusdt@trade"],
-        "id" : "1"
-      })");
-      m_wsc.subscribe(intent);
-      m_wsc.run();
-    }
+    TradeEventProducer(bb::EventQueue<T, 5> &eventQueue, bb::WebSocketClient<T> &webSocketClient);
 
   private:
     bb::EventQueue<T, 5> &m_eq;
     bb::WebSocketClient<T> &m_wsc;
     virtual void delegateEvent(const T &event) override;
+
+    static nlohmann::json intent;
   };
 } // bb
