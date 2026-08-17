61eaeee58d79bc6688cd20279c7e193b83dc4be4
felichio <fsafaridis@hotmail.com>
2026-08-18 02:01:22 +0300

    add also seconds as time duration

--- src/core/statistics/TimeLiterals.cpp
+++ src/core/statistics/TimeLiterals.cpp
@@ -1,16 +1,21 @@
 #include <core/statistics/TimeLiterals.hpp>
 
+uint64_t operator"" _s(unsigned long long seconds)
+{
+  return seconds * 1000;
+}
+
 uint64_t operator"" _m(unsigned long long minutes)
 {
-  return minutes * 60;
+  return operator""_s(minutes * 60);
 }
 
 uint64_t operator"" _h(unsigned long long hours)
 {
-  return  operator""_m(hours * 60);
+  return operator""_m(hours * 60);
 }
 
 uint64_t operator"" _d(unsigned long long days)
 {
-  return  operator""_h(days * 24);
+  return operator""_h(days * 24);
 }
--- src/core/statistics/TimeLiterals.hpp
+++ src/core/statistics/TimeLiterals.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstdint>
 
-
+uint64_t operator"" _s(unsigned long long seconds);
 uint64_t operator"" _m(unsigned long long minutes);
 uint64_t operator"" _h(unsigned long long hours);
 uint64_t operator"" _d(unsigned long long days);
